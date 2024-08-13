77fb2cb4b400f7247019fce38aef7c7b45999185
Fulvio <fulviodiniz0@gmail.com>
2024-08-13 11:56:14 -0300

    v2

--- app.py
+++ app.py
@@ -3,23 +3,65 @@ import os
 from werkzeug.utils import secure_filename
 from detection import detect_objects
 from ultralytics import YOLO
+from flask import send_from_directory
+import json
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads/'
 PROCESSED_FOLDER = 'static/processed/'
+INFO_FILE = 'image_info.json'  # Arquivo para armazenar informações sobre as imagens
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
 
-# Carregar o modelo YOLOv8 pré-treinado
 model = YOLO("yolov8m.pt")
-
-# Verifique se as pastas de upload e processed existem
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(PROCESSED_FOLDER, exist_ok=True)
 
+def save_image_detection_info(filename, count, colors):
+    """Salvar as informações de detecção de cada imagem em um arquivo JSON."""
+    info = {}
+    if os.path.exists(INFO_FILE):
+        with open(INFO_FILE, 'r') as f:
+            info = json.load(f)
+    
+    info[filename] = {
+        'count': count,
+        'colors': colors
+    }
+
+    with open(INFO_FILE, 'w') as f:
+        json.dump(info, f)
+
+def get_image_detection_info(filename):
+    """Recuperar as informações de detecção para uma imagem específica."""
+    if os.path.exists(INFO_FILE):
+        with open(INFO_FILE, 'r') as f:
+            info = json.load(f)
+            return info.get(filename, {'count': 0, 'colors': []})
+    return {'count': 0, 'colors': []}
+
+def get_latest_files_info(directory, limit=6):
+    """Função para obter os últimos arquivos processados com informações adicionais."""
+    files_info = []
+    files = os.listdir(directory)
+    files = [f for f in files if os.path.isfile(os.path.join(directory, f))]
+    files.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)), reverse=True)
+    
+    for filename in files[:limit]:
+        detection_info = get_image_detection_info(filename)
+        files_info.append({
+            'filename': filename,
+            'count': detection_info['count'],
+            'colors': detection_info['colors']
+        })
+    
+    return files_info
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Obtenha as últimas imagens processadas com suas informações
+    latest_files_info = get_latest_files_info(app.config['PROCESSED_FOLDER'])
+    return render_template('index.html', latest_files_info=latest_files_info)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -33,14 +75,17 @@ def upload_file():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
 
-       
-        processed_image_filename = detect_objects(file_path, app.config['PROCESSED_FOLDER'], model)
-        
-        return redirect(url_for('uploaded_file', filename=processed_image_filename))
+        # Detecte objetos na imagem e salve a imagem processada
+        processed_image_filename, count, colors = detect_objects(file_path, app.config['PROCESSED_FOLDER'], model)
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return render_template('index.html', filename=filename)
+        # Salve as informações de detecção para a imagem processada
+        save_image_detection_info(processed_image_filename, count, colors)
+
+        return redirect(url_for('index'))
+
+@app.route('/img/<path:filename>')
+def serve_images(filename):
+    return send_from_directory('templates/img', filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- detection.py
+++ detection.py
@@ -2,10 +2,10 @@ import os
 import cv2
 import numpy as np
 from ultralytics import YOLO
-from typing import Tuple
+from collections import Counter
+from typing import Tuple, List
 
 def load_image(image_path: str) -> Tuple[np.ndarray, np.ndarray]:
-    """Carrega uma imagem do caminho especificado."""
     if not os.path.isfile(image_path):
         raise FileNotFoundError(f"Arquivo não encontrado: {image_path}")
     
@@ -13,41 +13,51 @@ def load_image(image_path: str) -> Tuple[np.ndarray, np.ndarray]:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img, img_rgb
 
-def draw_detections(img: np.ndarray, boxes, model: YOLO) -> np.ndarray:
-    """Desenha caixas delimitadoras na imagem para os objetos detectados."""
+def draw_detections(img: np.ndarray, boxes, model: YOLO) -> Tuple[np.ndarray, int, List[str]]:
+    count = 0
+    colors = []
     for box in boxes:
         class_id = int(box.cls)
         class_name = model.names[class_id]
         confidence = box.conf.item()
-        bbox = box.xyxy[0].tolist()  # Converter tensor para lista
+        bbox = box.xyxy[0].tolist()
 
-        if class_name in ['cow', 'bull']:  # Filtrar para detectar vacas e bois
+        if class_name in ['cow', 'bull']:
+            count += 1
             x1, y1, x2, y2 = map(int, bbox)
             label = f"{class_name} {confidence:.2f}"
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
             cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-    return img
+            
+            roi = img[y1:y2, x1:x2]
+            dominant_color = get_dominant_color(roi)
+            colors.append(dominant_color)
+    
+    return img, count, colors
+
+def get_dominant_color(image: np.ndarray) -> str:
+    pixels = np.float32(image.reshape(-1, 3))
+    n_colors = 5
+    _, labels, palette = cv2.kmeans(pixels, n_colors, None, 
+                                    (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, 0.1), 
+                                    10, cv2.KMEANS_RANDOM_CENTERS)
+    counts = Counter(labels.flatten())
+    dominant_color = palette[np.argmax(counts)]
+    
+    return f'RGB({int(dominant_color[0])}, {int(dominant_color[1])}, {int(dominant_color[2])})'
 
 def resize_image(image: np.ndarray, width: int) -> np.ndarray:
-    """Redimensiona a imagem mantendo a proporção."""
     height = int(image.shape[0] * (width / image.shape[1]))
     return cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
 
-def detect_objects(image_path: str, output_folder: str, model: YOLO, conf_thres: float = 0.25, imgsz: int = 640) -> str:
-    """Detecta objetos em uma imagem e salva a imagem com detecções desenhadas."""
+def detect_objects(image_path: str, output_folder: str, model: YOLO, conf_thres: float = 0.25, imgsz: int = 640) -> Tuple[str, int, List[str]]:
     img, img_rgb = load_image(image_path)
-    
-    # Realizar a detecção usando o modelo YOLOv8 com parâmetros ajustados
     results = model.predict(img_rgb, conf=conf_thres, imgsz=imgsz)
     boxes = results[0].boxes
     
-    # Desenhar caixas delimitadoras na imagem original
-    img_with_detections = draw_detections(img, boxes, model)
-    
-    # Redimensionar a imagem
+    img_with_detections, count, colors = draw_detections(img, boxes, model)
     img_with_detections = resize_image(img_with_detections, width=800)
     
-    # Salvar a imagem com as detecções
     output_path = os.path.join(output_folder, 'processed_' + os.path.basename(image_path))
     cv2.imwrite(output_path, img_with_detections)
-    return os.path.basename(output_path)
+    return os.path.basename(output_path), count, colors
